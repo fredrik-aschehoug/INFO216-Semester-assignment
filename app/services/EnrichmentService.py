d4ed8e51cbb220ad35920de87fce14a4e7d979f3
fredrik-aschehoug <15358786+fredrik-aschehoug@users.noreply.github.com>
2021-04-10 15:23:06 +0200

    Refactor EnrichmentService

--- app/services/EnrichmentService.py
+++ app/services/EnrichmentService.py
@@ -1,16 +1,21 @@
+from config.config import settings
+from models.models import Item
 from rdflib import URIRef
 from rdflib.namespace import OWL
+
+from services.AsyncService import AsyncService
 from services.GraphService import GraphService
-from services.YagoService import YagoService
-from services.WikidataService import WikidataService
 from services.RelationService import RelationService
 from services.UriService import UriService
-from services.AsyncService import AsyncService
-from models.models import Item
-from config.config import settings
+from services.WikidataService import WikidataService
+from services.YagoService import YagoService
 
 
 class EnrichmentService(AsyncService):
+    """
+    Service used to orchestrate the enrichment process.
+    This class should be injected as a dependency in a FastAPI route.
+    """
 
     def __init__(self, params: Item):
         # Register services
@@ -20,46 +25,40 @@ class EnrichmentService(AsyncService):
         self.uriService = UriService()
         self.relationService = RelationService(self.graphService.graph)
 
-    def get_response(self) -> str:
-        return {"graph": self.graphService.get_graph_serialized(), "notation": self.graphService.notation}
+    async def _add_external_triples(self, external_uri: str, entity: str, service) -> None:
+        """Get triples related to external_uri from the service and add them to the graph"""
+        triples = await service.get_triples(external_uri)
 
-    async def add_yago_triples(self, yago_uri, entity) -> None:
-        triples = await self.yagoService.get_triples(yago_uri)
         if (len(triples) == 0):
             return
 
-        subject = triples[0]["subject"]
+        # Add triple: nhterm:Entity owl:sameAs external:Entity
+        external_subject = triples[0]["subject"]
+        self.graphService.add((URIRef(entity), OWL.sameAs, self.graphService.create_node(external_subject)))
 
-        # Add triple: nhterm:Entity owl:sameAs yago3:Entity
-        self.graphService.add((URIRef(entity), OWL.sameAs, self.graphService.create_node(subject)))
-
-        # Add triples returned from Yago3
+        # Add triples returned from service to the graph
         for triple in triples:
             self.graphService.add(self.graphService.create_triple(triple))
 
-    async def extend_yago(self) -> None:
-        tasks = [self.add_yago_triples(uri_map["yago"], uri_map["entity"]) for uri_map in self.uriService.maps]
+    async def _extend_yago(self) -> None:
+        """For each yago4 uri in the uriService, fetch related triples from yago4 endpoint and add them to the graph."""
+        tasks = [self._add_external_triples(uri_map["yago"], uri_map["entity"], self.yagoService) for uri_map in self.uriService.maps]
         await self.gather_with_concurrency(settings.yago_endpoint_max_connections, tasks)
 
-    async def add_wd_triples(self, subject_uri) -> None:
-        triples = await self.wikidataService.get_triples(subject_uri)
-        for triple in triples:
-            self.graphService.add(self.graphService.create_triple(triple))
-
-    async def extend_wd(self) -> None:
-        for uri_map in self.uriService.maps:
-            if (uri_map["wd"] is not None):
-                self.graphService.add((URIRef(uri_map["entity"]), OWL.sameAs, URIRef(uri_map["wd"])))
-
-        # Do concurrent request, but only 5 at a time
-        tasks = [self.add_wd_triples(uri_map["wd"]) for uri_map in self.uriService.maps]
+    async def _extend_wd(self) -> None:
+        """For each wikidata uri in the uriService, fetch related triples from wikidata endpoint and add them to the graph."""
+        tasks = [self._add_external_triples(uri_map["wd"], uri_map["entity"], self.wikidataService) for uri_map in self.uriService.maps]
         await self.gather_with_concurrency(settings.wd_endpoint_max_connections, tasks)
 
     async def extend(self) -> None:
         self.uriService.add_entities(self.graphService.get_entities())
         await self.uriService.add_yago_uris(self.yagoService.get_yago_URI)
         await self.uriService.add_wd_uris(self.yagoService.get_wd_URI)
-        await self.gather([self.extend_yago(), self.extend_wd()])
+        await self.gather([self._extend_yago(), self._extend_wd()])
 
     def annotate_relations(self) -> None:
         self.relationService.annotate_relations()
+
+    def get_response(self) -> str:
+        """Create the response to be returned after the enrichment."""
+        return {"graph": self.graphService.get_graph_serialized(), "notation": self.graphService.notation}
