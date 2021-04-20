ae74f01c65f989bdc571677b91b15f7b50fe1dd2
fredrik-aschehoug <15358786+fredrik-aschehoug@users.noreply.github.com>
2021-04-20 12:19:22 +0200

    Update main

--- app/main.py
+++ app/main.py
@@ -1,15 +1,18 @@
 from fastapi import FastAPI, Depends
-from models.models import Item
+from models.models import ResponseModel
 from services.EnrichmentService import EnrichmentService
-# from fastapi_cprofile.profiler import CProfileMiddleware
 
 app = FastAPI()
-# app.add_middleware(CProfileMiddleware, enable=True, print_each_request=True, strip_dirs=True, sort_by='cumulative')
+
+# Uncomment this to enable profiling
+# from fastapi_cprofile.profiler import CProfileMiddleware
+# app.add_middleware(CProfileMiddleware, enable=True, print_each_request=True, strip_dirs=True, sort_by='cumtime')
 
 
-@app.get("/", response_model=Item)
+@app.get("/", response_model=ResponseModel)
 async def get_extended_graph(encricher: EnrichmentService = Depends(EnrichmentService)):
     await encricher.extend()
+
     encricher.annotate_relations()
 
     return encricher.get_response()
